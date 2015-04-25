37e69522ccb0f4db2482b3e3f0fa71c6f7b57967
Brandon Kirklen <brandonkirklen@gmail.com>
2015-04-25 12:33:11 -0700

    Polygon Init

---
+++ Lab4/src/Polygon/Polygon.java
@@ -0,0 +1,50 @@
+package Polygon;
+
+/**
+ * Created by Brandon Kirklen on 4/25/2015.
+ *  A polygon is a closed curve made up from line segments
+ that join the corner points.
+ */
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+
+public class Polygon
+{
+
+
+    /**
+     Constructs a polygon with no corner points.
+     */
+    public Polygon()
+    {
+
+    }
+
+    /**
+     Adds a corner point to the polygon.
+     @param x the x-coordinate of the point
+     @param y the y-coordinate of the point
+     */
+    public void add(double x, double y)
+    {
+
+    }
+
+    /**
+     Calculates the perimeter of a polygon.
+     @return the perimeter of this polygon
+     */
+    public double perimeter()
+    {
+        return 0;
+    }
+
+    /**
+     Calculates the area of a polygon.
+     @return the area of this polygon
+     */
+    public double area()
+    {
+        return 0;
+    }
+}
---
+++ Lab4/src/Polygon/PolygonTester.java
@@ -0,0 +1,44 @@
+package Polygon;
+
+/**
+ * Created by Brandon Kirklen on 4/25/2015.
+ This is a tester class for Polygon.
+ */
+public class PolygonTester
+{
+    public static void main(String[] args)
+    {
+        Polygon square = new Polygon();
+
+        square.add(100, 100);
+        square.add(100, 150);
+        square.add(150, 150);
+        square.add(150, 100);
+
+        Polygon hexagon = new Polygon();
+
+        // construct a regular hexagon
+        for (int i = 0; i < 6; i++)
+        {
+            double x = 5*i;
+            double y = 3*i;
+            hexagon.add(x, y);
+        }
+
+        double squarePerimeter = square.perimeter();
+        System.out.println("Square perimeter: " + squarePerimeter);
+        System.out.println("Expected: . . .");
+
+        double squareArea = square.area();
+        System.out.println("Square area: " + squareArea);
+        System.out.println("Expected: . . .");
+
+        double hexagonPerimeter = hexagon.perimeter();
+        System.out.println("Hexagon perimeter: " + hexagonPerimeter);
+        System.out.println("Expected: . . .");
+
+        double hexagonArea = hexagon.area();
+        System.out.println("Hexagon area: " + hexagonArea);
+        System.out.println("Expected: . . .");
+    }
+}
