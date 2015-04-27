1d756ad081e7c3f5470cc6ed189b4da74b3de51a
Brandon Kirklen <brandonkirklen@gmail.com>
2015-04-27 03:57:27 -0700

    Pie Chart Simiworking

--- Lab4/src/PieChart/PieChart.java
+++ Lab4/src/PieChart/PieChart.java
@@ -1,17 +1,40 @@
 package PieChart;
 
 import java.awt.*;
+import java.awt.geom.Line2D;
+import java.util.ArrayList;
 
 /**
  * Created by Brandon Kirklen on 4/27/2015.
+ * PieChart
  */
 public class PieChart {
+    ArrayList<Double> valuesCollection = new ArrayList<>();
+    int width, height;
+    public PieChart(int width, int height)
+    {
+        this.width=width;
+        this.height=height;
+    }
     public void add(double value)
     {
-
+        valuesCollection.add(value);
     }
     public void draw(Graphics2D g2)
     {
+        g2.drawOval(0, 0, width, height);
+        double sum = 0;
+        for(double d : valuesCollection)
+        {
+            sum += d;
+        }
+        for (int i=0; i < valuesCollection.size()-1; i++)
+        {
+            Line2D.Double line = new Line2D.Double(width/2, height/2,
+                    width/2 + width/2*Math.cos((valuesCollection.get(i)/sum)*100*2*Math.PI),
+                    height/2 + height/2*Math.sin((valuesCollection.get(i)/sum)*100*2*Math.PI));
+            g2.draw(line);
+        }
 
     }
 }
