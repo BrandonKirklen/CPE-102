3433b9bd30d5b4c312f436de9e953e2196fe3d13
Brandon Kirklen <brandonkirklen@gmail.com>
2015-04-28 02:08:59 -0700

    sum init, todo: logic

--- Lab4/src/Square/Square.java
+++ Lab4/src/Square/Square.java
@@ -12,8 +12,47 @@ public class Square {
     {
         magicNumbers.add(i);
     }
+    private boolean checkFrequency()
+    {
+        boolean noRepeats = true;
+        int[] freq = new int[magicNumbers.size()];
+        for (int i = 0; i < magicNumbers.size(); i++)
+        {
+            freq[magicNumbers.get(i)] += 1;
+        }
+
+        for (int i = 0; i < freq.length; i++)
+        {
+            if(freq[i] != 1)
+            {
+                noRepeats = false;
+            }
+        }
+        return noRepeats;
+    }
+    private boolean checkSum()
+    {
+        boolean goodSum = true;
+        int size = (int)Math.sqrt( (double)magicNumbers.size());
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = i*size; j < (i+1)*size; j++)
+            {
+                int sum=0;
+                sum += magicNumbers.get(j);
+            }
+        }
+        return false;
+    }
     public boolean isMagic()
     {
-        return false;
+        if (checkFrequency())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
